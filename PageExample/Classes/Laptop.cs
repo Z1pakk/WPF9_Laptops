1e3a4ba4bb8b4b1c4d924ae160a0439a012f6151
Vlad <Vlad@DESKTOP-HJ9OVGE>
2019-12-03 20:32:42 +0200

    Initilize project

---
+++ PageExample/Classes/Laptop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace PageExample.Classes
+{
+    public class Laptop
+    {
+        public Laptop() {}
+
+        public Laptop(string name, float price, string imagePath)
+        {
+            this.Name = name;
+            this.Price = price;
+            string path = Path.Combine(
+                Environment.CurrentDirectory, 
+                ConfigurationManager.AppSettings["LaptopImages"].ToString());
+
+            string pathImage = Path.Combine(path, imagePath);
+
+            this.Image = new BitmapImage(new Uri(pathImage, UriKind.Absolute));
+        }
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public BitmapImage Image { get; set; }
+    }
+}
---
+++ PageExample/Pages/NotebooksPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using PageExample.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PageExample.Pages
+{
+    /// <summary>
+    /// Interaction logic for NotebooksPage.xaml
+    /// </summary>
+    public partial class NotebooksPage : Page
+    {
+        public NotebooksPage()
+        {
+            InitializeComponent();
+            List<Laptop> laptops = new List<Laptop>
+            {
+                new Laptop("Супер ноутбук 3000", 3000 , "dog.jpg"),
+                new Laptop("Супер ноутбук 3", 6000 , "notebook1.webp"),
+                new Laptop("Супер ноутбук 4", 4000 , "dog.jpg"),
+                new Laptop("Супер ноутбук 5", 3000 , "dog.jpg")
+            };
+
+            lvNotebooks.ItemsSource = laptops;
+        }
+    }
+}
